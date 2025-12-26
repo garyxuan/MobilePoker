82127e15aec483ab360e0bd0a8c4a2cf0044aab8
wangxiaoxuan <garyxuan@163.com>
2025-12-26 17:27:50 +0800

    Initial Commit

---
+++ MobilePoker/MobilePokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilePokerApp.swift
+//  MobilePoker
+//
+//  Created by garyxuan on 2025/12/26.
+//
+
+import SwiftUI
+
+@main
+struct MobilePokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
